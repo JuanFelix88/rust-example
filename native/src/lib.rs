34949d7d02015247b23dee058c19b8cb8206d5ee
Juan Felix <asxy.2015@gmail.com>
2020-01-23 21:25:41 +0000

    resolve in new thread

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -2,22 +2,33 @@ extern crate neon;
 // {JsValue, Value, JsObject, JsArray, JsFunction, JsBoolean, JsNumber, JsString, StringResult, JsNull, JsUndefined}
 
 use neon::prelude::*;
-use std::fs;
+use neon::types;
+
+use std::{fs, thread};
 
 static mut AMOUNT: u8 = 0;
 
-
 fn hello(mut cx: FunctionContext) -> JsResult<JsNumber> {
-    unsafe {
-        let data: Vec<u8> = fs::read("data.bin").expect("File not avaible!");
-        for item in data {
-            if item == 0 {
-                AMOUNT += 1;
-            }
-        }
+unsafe 
+{
+    let mut data: Vec<u8> = fs::read("native/index.node").expect("File not avaible!");
+    let length: usize = data.len() - 1;
+    let mut codex: Vec<u8> = vec![];
 
-        Ok(cx.number(AMOUNT))
-    }
+    // codex.copy_from_slice(&datax[4187191..]);
+    let handler = thread::spawn(move || {
+        for index in (4465167..length) {
+            codex.push(data[index]);
+            AMOUNT += 1;
+        }
+    });
+
+    handler.join().unwrap();
+
+    let lenx = codex.len();
+
+    Ok(cx.number(lenx as f64))
+}
 }
 
 register_module!(mut cx, {
