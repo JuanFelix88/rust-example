00bdd4457cf3fe03686e80fb8a18d3e47e75f9e3
Juan Felix <asxy.2015@gmail.com>
2020-01-23 16:25:41 +0000

    add read data.bin

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,11 +1,21 @@
-#[macro_use]
 extern crate neon;
 
 use neon::prelude::*;
+use std::fs;
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    let tap = "Test addon";
-    Ok(cx.string(tap))
+static mut AMOUNT: u8 = 0;
+
+fn hello(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    unsafe {
+        let data: Vec<u8> = fs::read("data.bin").expect("File not avaible!");
+        for item in data {
+            if item == 0 {
+                AMOUNT += 1;
+            }
+        }
+
+        Ok(cx.number(AMOUNT))
+    }
 }
 
 register_module!(mut cx, {
